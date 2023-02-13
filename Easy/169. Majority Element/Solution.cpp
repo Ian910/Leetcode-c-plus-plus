024628859e819a29a99069c6d678a897b58f0556
Ian910 <k234954693@gmail.com>
2023-02-13 22:54:13 +0800

    Majority Element

---
+++ Easy/169. Majority Element/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+        int ans = arr[0], count = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (count == 0)ans = arr[i];
+            count += (ans == arr[i]) ? 1 : -1;
+        }
+        return ans;
+    }
+};
