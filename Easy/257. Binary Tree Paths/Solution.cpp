2078c8f0a28ad25ce7ce22a6d024251d6e82431d
Ian910 <k234954693@gmail.com>
2023-02-16 20:51:33 +0800

    Update Solution.cpp

--- Easy/257. Binary Tree Paths/Solution.cpp
+++ Easy/257. Binary Tree Paths/Solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        int size = s.size(), i;
-        if (size != t.size()) return false;
-        unordered_map<char, int> mp;
-        for (i = 0; i < size; i++) {
-            mp[s[i]]++;
-            mp[t[i]]--;
-        }
-        for (auto it : mp)
-            if (it.second != 0) return false;
-        return true;
+    vector<string> binaryTreePaths(TreeNode* root) {
+        if (root == NULL)return {};
+        vector<string>ans;
+        string root_val = to_string(root->val);
+        vector<string>left = binaryTreePaths(root->left);
+        vector<string>right = binaryTreePaths(root->right);
+        for (auto it : left)ans.push_back(root_val + "->" + it);
+        for (auto it : right)ans.push_back(root_val + "->" + it);
+        if (ans.size() == 0)return{ root_val };
+        return ans;
     }
 };
