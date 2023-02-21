1e4af8d8bc261f2ee39ee857335571ea1b07c9ab
Ian910 <k234954693@gmail.com>
2023-02-21 22:16:10 +0800

    374. Guess Number Higher or Lower

--- Easy/374. Guess Number Higher or Lower/Solution.cpp
+++ Easy/374. Guess Number Higher or Lower/Solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    bool isPerfectSquare(int num) {
-        if (num == 1) return true;
-        long int min = 1, max = num, mid, temp;
+    int guessNumber(int n) {
+        long int min = 1, max = n, mid;
+        int temp;
         while (min <= max) {
             mid = min + (max - min) / 2;
-            temp = mid * mid;
-            if (temp == num) return true;
-            else if (temp > num) max = mid - 1;
-            else min = mid + 1;
+            temp = guess(mid);
+            if (temp == 0) return mid;
+            else if (temp == 1) min = mid + 1;
+            else max = mid - 1;
         }
-        return false;
+        return 1;
     }
 };
